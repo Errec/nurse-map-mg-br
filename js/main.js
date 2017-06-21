edb2f515f846dd0aa7c89f475c42dc0f9ace300b
Raniro Coelho <errecme@gmail.com>
2017-06-20 23:50:32 -0300

    Add char.js bars to canvas

---
+++ js/main.js
@@ -0,0 +1,49 @@
+var barChartData = {
+    labels: ["Total", "Enfermeiros", "Tecnicos", "Auxiliares"],
+    datasets: [{
+        label: 'Região',
+        backgroundColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+            ],
+        data: [32, 19, 3, 5]
+    }, {
+        label: 'Estado',
+        backgroundColor: [
+                'rgba(255,99,132,0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+            ],
+        data: [501, 192, 31, 55]
+    }]
+
+};
+window.onload = function() {
+    var ctx = document.getElementById("canvas").getContext("2d");
+    myBar = new Chart(ctx, {
+        type: 'bar',
+        data: barChartData,
+        options: {
+            title:{
+                display:false
+            },
+            tooltips: {
+                mode: 'index',
+                intersect: false
+            },
+            responsive: true,
+            maintainAspectRatio: false,
+            scales: {
+                xAxes: [{
+                    stacked: true,
+                }],
+                yAxes: [{
+                    stacked: true
+                }]
+            }
+        }
+    });
+};
