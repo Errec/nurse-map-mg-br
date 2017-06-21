12e5a691cca2053dd9a789fe004b7c099bc5d188
Raniro Coelho <errecme@gmail.com>
2017-06-21 11:50:45 -0300

    Make _render update table values

--- js/map-click.js
+++ js/map-click.js
@@ -1,6 +1,15 @@
-var svgHover = (function(){
+var svgClick = (function(){
   // Cache the DOM
-  var map = document.getElementById("map");
+  var map   = document.getElementById("map");
+  var title = document.getElementById("region-title");
+  var nprof = document.getElementById("td-prof-n");
+  var pprof = document.getElementById("td-prof-p");
+  var nenf  = document.getElementById("td-enf-n");
+  var penf  = document.getElementById("td-enf-p");
+  var ntec  = document.getElementById("td-tec-n");
+  var ptec  = document.getElementById("td-tec-p");
+  var naux  = document.getElementById("td-aux-n");
+  var paux  = document.getElementById("td-aux-p");
 
   //  Bind mouse events
   map.addEventListener("click", _showData, false);
@@ -12,8 +21,18 @@ var svgHover = (function(){
     e.stopPropagation();
   }
 
-  function _render(region) {
-    console.log(regionData.macroRegioes[region]);
+  function _render(regionName) {
+    console.log(regionData.macroRegioes[regionName]);
+    var dataRegion  = regionData.macroRegioes[regionName];
 
+    title.innerHTML = regionName;
+    nprof.innerHTML = dataRegion.profissionais;
+    pprof.innerHTML = (dataRegion.profissionais / regionData.totalProfissionais * 100).toFixed(2) + '%';
+    nenf.innerHTML  = dataRegion.enfermeiros;
+    penf.innerHTML  = (dataRegion.enfermeiros / regionData.totalEfenfermeiros * 100).toFixed(2) + '%';
+    ntec.innerHTML  = dataRegion.tecnicos;
+    ptec.innerHTML  = (dataRegion.tecnicos / regionData.totalTecnicos * 100).toFixed(2) + '%';
+    naux.innerHTML  = dataRegion.auxiliares;
+    paux.innerHTML  = (dataRegion.auxiliares / regionData.totalAuxiliares * 100).toFixed(2) + '%';
   }
 })();
