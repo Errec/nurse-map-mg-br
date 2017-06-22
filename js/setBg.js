3106f67097132505e913896d608f2b5041288770
Raniro Coelho <errecme@gmail.com>
2017-06-22 15:25:03 -0300

    Add macro region to setBg

--- js/setBg.js
+++ js/setBg.js
@@ -1,8 +1,9 @@
 var setBg = (function(){
   // Cache the DOM
-  var map = document.getElementById("microrreg");
+  var microRegion = document.getElementById("microrreg");
+  var macroRegion = document.getElementById("macrorreg");
 
-  Array.prototype.forEach.call(map.children, function(el) {
+  Array.prototype.forEach.call(microRegion.children, function(el) {
     var professionals = regionData.microRegioes[el.dataset.name].profissionais;
     switch(true) {
       case professionals < 1000:
@@ -18,6 +19,30 @@ var setBg = (function(){
         el.classList.add("bg-4");
         break;
       case professionals < 10000:
+        el.classList.add("bg-5");
+        break;
+      default:
+        el.classList.add("bg-6");
+        break;
+    }
+  });
+
+  Array.prototype.forEach.call(macroRegion.children, function(el) {
+    var professionals = regionData.macroRegioes[el.dataset.name].profissionais;
+    switch(true) {
+      case professionals < 3000:
+        el.classList.add("bg-1");
+        break;
+      case professionals < 5000:
+        el.classList.add("bg-2");
+        break;
+      case professionals < 10000:
+        el.classList.add("bg-3");
+        break;
+      case professionals < 15000:
+        el.classList.add("bg-4");
+        break;
+      case professionals < 30000:
         el.classList.add("bg-5");
         break;
       default:
